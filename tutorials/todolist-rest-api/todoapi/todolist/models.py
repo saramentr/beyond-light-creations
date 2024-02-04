809fa4db6c8fb7afb5d4a99814c9232d9ee814cb
Sara Mentr <saramentr@gmail.com>
2024-02-04 09:59:17 +0300

    Update models.py

--- tutorials/todolist-rest-api/todoapi/todolist/models.py
+++ tutorials/todolist-rest-api/todoapi/todolist/models.py
@@ -1,28 +1,35 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Todo(models.Model):
-  id = models.AutoField(
-    primary_key=True
-  )
+    result = models.CharField(max_length=50)
+    result_id = models.UUIDField(
+        primary_key = True, 
+        default = uuid.uuid4,
+        editable = False, 
+        unique=True
+        )
 
-  text = models.TextField(
-    max_length=1000,
-    null=False,
-    blank=False
-  )
+    status = models.CharField(max_length=50)
+    timeout = models.PositiveIntegerField(default=0)
+  
+    type = models.TextField(
+        max_length=1000,
+        null=False,
+        blank=False
+    )
+    creation_date = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=False
+    )
 
-  creation_date = models.DateTimeField(
-    auto_now_add=True,
-    null=False,
-    blank=False
-  )
-
-  last_updated = models.DateTimeField(
-    auto_now=True,
-    null=False,
-    blank=False
-  )
+    last_updated = models.DateTimeField(
+        auto_now=True,
+        null=False,
+        blank=False
+    )
 
   class Meta:
     db_table = 'Todos'
